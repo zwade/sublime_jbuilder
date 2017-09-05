9b1f4e4f12739a84c36b0414876ac0e27800ebe8
Zachary Wade <zacharywade@gmail.com>
2017-09-05 14:22:29 -0400

    Tue Sep  5 14:22:29 EDT 2017: Pushing to sublime

--- jbuilder.py
+++ jbuilder.py
@@ -1,14 +1,21 @@
 import os
 import subprocess
 import sublime, sublime_plugin
+import threading
 
 base_directory = os.path.dirname(os.path.realpath(__file__))
 find_targets_exe = os.path.join(base_directory, "_build", "default", "find_targets", "find_targets.exe")
 
-def reload_if_needed(force=False):
-	if (force 
+class find_targets(threading.thread):
+	def needs_reload(self, force=False):
+		return (force 
 			or not os.path.isfile(find_targets_exe)
-			or ("BUILD_ON_RELOAD" in os.environ and os.environ["BUILD_ON_RELOAD"])):
+			or ("BUILD_ON_RELOAD" in os.environ and os.environ["BUILD_ON_RELOAD"]))
+	def reload_if_needed(self, force=False):
+		if self.needs_reload(force=force):
+			self.start()
+
+	def run():
 		os.chdir(base_directory)
 		print("Rebuilding find_targets in directory {}".format(base_directory))
 		proc = subprocess.Popen (["jbuilder", "build", "find_targets/find_targets.exe"], 
