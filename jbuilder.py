46cee4fcd21cca47ae2569b5e637a2e697b2729e
Zachary Wade <zacharywade@gmail.com>
2017-09-05 14:27:50 -0400

    Tue Sep  5 14:27:50 EDT 2017: Pushing to sublime

--- jbuilder.py
+++ jbuilder.py
@@ -6,16 +6,16 @@ import threading
 base_directory = os.path.dirname(os.path.realpath(__file__))
 find_targets_exe = os.path.join(base_directory, "_build", "default", "find_targets", "find_targets.exe")
 
-build_lock = threading.Lock()
 
-class Find_targets(threading.Thread):
+class Find_targets_builder(threading.Thread):
+	build_lock = threading.Lock()
 	def needs_reload(self, force=False):
 		return (force 
 			or not os.path.isfile(find_targets_exe)
 			or ("BUILD_ON_RELOAD" in os.environ and os.environ["BUILD_ON_RELOAD"]))
 
 	def run(self):
-		build_lock.acquire()
+		Find_targets_builder.build_lock.acquire()
 		os.chdir(base_directory)
 		print("Rebuilding find_targets in directory {}".format(base_directory))
 		proc = subprocess.Popen (["jbuilder", "build", "find_targets/find_targets.exe"], 
@@ -29,13 +29,12 @@ class Find_targets(threading.Thread):
 			print(proc.stderr.read().decode("utf-8"))
 		else:
 			print("jbuilder succeeded")
-		build_lock.release()
+		Find_targets_builder.build_lock.release()
 
 def reload_if_needed(force=False):
 	target = Find_targets()
 	if target.needs_reload(force):
 		target.start()
-		print("Dispatched thread")
 
 reload_if_needed(force=True)
 
