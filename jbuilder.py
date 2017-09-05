458b3d182ba5ce663af6f1ffb3e2d645cf2cc6c0
Zachary Wade <zacharywade@gmail.com>
2017-09-05 13:19:27 -0400

    Tue Sep  5 13:19:27 EDT 2017: Pushing to sublime

--- jbuilder.py
+++ jbuilder.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import sublime, sublime_plugin
 
+
 base_directory = os.path.dirname(os.path.realpath(__file__))
 find_targets_exe = os.path.join(base_directory, "_build", "default", "find_targets", "find_targets.exe")
 
@@ -11,15 +12,13 @@ def reload_if_needed(force=False):
 			or ("BUILD_ON_RELOAD" in os.environ and os.environ["BUILD_ON_RELOAD"])):
 		os.chdir(base_directory)
 		print("Rebuilding find_targets in directory {}".format(base_directory))
-		proc = subprocess.Popen ("jbuilder build find_targets/find_targets.exe", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+		proc = subprocess.Popen (["jbuilder", "build", "find_targets/find_targets.exe"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		return_code = proc.wait()
 		if return_code != 0:
 			print("jbuilder failed with:")
 			print(proc.stderr.read().decode("utf-8"))
 		else:
 			print("jbuilder succeeded")
-			print(proc.stdout.read().decode("ascii"))
-			print(proc.stderr.read().decode("ascii"))
 
 
 reload_if_needed()
