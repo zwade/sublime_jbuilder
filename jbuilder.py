325373fd8a1092a47758caa0a2e1629b3943af96
Zachary Wade <zacharywade@gmail.com>
2017-09-07 13:56:43 -0400

    Thu Sep  7 13:56:43 EDT 2017: Pushing to sublime

--- jbuilder.py
+++ jbuilder.py
@@ -96,7 +96,7 @@ class JbuilderCmd(sublime_plugin.WindowCommand):
 
 	def run(self, cmd):
 		folder = self.window.folders()[0] if len(self.window.folders()) > 0 else "."
-		print(folder)
+		print("Current folder is: {}".format(folder))
 		find_targets = Find_targets()
 		targets = [y for (x,y) in find_targets.list()]
 		print(cmd)
